1efb28c87faa23477e54654cb6e39e49167dca8c
PatrycjaSmol <54121426+PatrycjaSmol@users.noreply.github.com>
2021-12-04 10:57:28 +0100

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,35 @@
+import json
+
+points = 0
+
+def show_question(question):
+    global points
+    print()
+    print(question["pytanie"])
+    print("a:", question["a"])
+    print("b:", question["b"])
+    print("c:", question["c"])
+    print("d:", question["d"])
+
+
+    print()
+
+    answer = input("Którą odpowiedź wybierasz? ")
+
+    if answer == question["prawidlowa_odpowiedz"]:
+        points += 10
+        print("To prawidłowa odpowiedź, brawo! Masz już", points, "punktów!")
+        
+    else:
+        print()
+        print("Niestety to zła odpowiedź, prawidłowa odpowiedź to", question["prawidlowa_odpowiedz"])
+        
+        
+with open ("quiz.json") as json_file:
+    questions = json.load(json_file)
+
+for i in range(0, len(questions)):
+  show_question(questions[i])
+
+print()
+print("To koniec gry :( Ilość zdobytych przez Ciebie punktów to " + str(points) + ".")
